2517d5d840ba7aa98cec25f32155c9609235564b
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-01-11 12:37:50 +0300

    Added RxJS and change added functionality to automatic update

--- js/models/Image.js
+++ js/models/Image.js
@@ -1,14 +1,17 @@
+import Rx from 'rxjs/Rx';
+
 export class Image {
 
     constructor(link) {
         this.link = link;
+        this.subject = new Rx.BehaviorSubject('');
         this.images = JSON.parse(localStorage.getItem('images')) || [];
     }
 
-    addImage(data, cb) {
+    addImage(data) {
         this.images.push(data);
         localStorage.setItem('images', JSON.stringify(this.images));
-        cb(true);
+        this.subject.next();
     }
 
     getImages() {
